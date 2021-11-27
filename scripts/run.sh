88555e1d4d6034dd0fc45cc0e3a6fa006b4de645
Christopher Keith <chris.keith@gmail.com>
2021-11-27 10:52:56 -0800

    Add internal class.

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,4 +2,4 @@ set -x
 
 cd ~/Documents/Github/npi-notify-clients-of-court-dates/				; if [ $? -ne 0 ] ; then exit -6 ; fi
 
-mvn clean install exec:java
+mvn clean install
--- src/test/java/ScrapeDataTest.java
+++ src/test/java/ScrapeDataTest.java
@@ -8,14 +8,45 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.JavascriptExecutor;
 import java.util.*;
 public class ScrapeDataTest {
+
+  public class Scraper {
+    WebDriver driver;
+    private Map<String, Object> vars;
+    public Scraper(WebDriver driver) {
+      this.driver = driver;
+      this.vars = new HashMap<String, Object>();
+    }
+    public void run() {
+      driver.get("https://gscivildata.shelbycountytn.gov/pls/gnweb/ck_public_qry_cpty.cp_personcase_setup_idx");
+      driver.switchTo().frame(1);
+      driver.findElement(By.name("partial_ind")).click();
+      driver.findElement(By.name("last_name")).click();
+      driver.findElement(By.name("last_name")).sendKeys("A");
+      driver.findElement(By.name("begin_date")).click();
+      driver.findElement(By.name("begin_date")).sendKeys("01-NOV-2021");
+      driver.findElement(By.cssSelector("tr:nth-child(7) > td:nth-child(1)")).click();
+      driver.findElement(By.name("end_date")).click();
+      driver.findElement(By.name("end_date")).sendKeys("14-NOV-2021");
+      driver.findElement(By.name("case_type")).click();
+  
+      driver.findElement(By.cssSelector("option:nth-child(17)")).click();
+      driver.findElement(By.cssSelector("input:nth-child(4)")).click();
+      driver.switchTo().frame(1);
+  
+      driver.findElement(By.linkText("2109558")).click();
+      driver.switchTo().frame(1);
+      vars.put("courtDate", driver.findElement(By.cssSelector("a:nth-child(5) td:nth-child(2)")).getText());
+      vars.put("room", driver.findElement(By.cssSelector("a:nth-child(5) td:nth-child(3)")).getText());
+      vars.put("location", driver.findElement(By.cssSelector("a:nth-child(5) td:nth-child(4)")).getText());
+      System.out.println(vars.get("courtDate").toString());
+      }
+  }
+
   private WebDriver driver;
-  private Map<String, Object> vars;
   JavascriptExecutor js;
   @Before
   public void setUp() {
     driver = new ChromeDriver();
-    js = (JavascriptExecutor) driver;
-    vars = new HashMap<String, Object>();
   }
   @After
   public void tearDown() {
@@ -23,27 +54,6 @@ public class ScrapeDataTest {
   }
   @Test
   public void scrapeData() {
-    driver.get("https://gscivildata.shelbycountytn.gov/pls/gnweb/ck_public_qry_cpty.cp_personcase_setup_idx");
-    driver.switchTo().frame(1);
-    driver.findElement(By.name("partial_ind")).click();
-    driver.findElement(By.name("last_name")).click();
-    driver.findElement(By.name("last_name")).sendKeys("A");
-    driver.findElement(By.name("begin_date")).click();
-    driver.findElement(By.name("begin_date")).sendKeys("01-NOV-2021");
-    driver.findElement(By.cssSelector("tr:nth-child(7) > td:nth-child(1)")).click();
-    driver.findElement(By.name("end_date")).click();
-    driver.findElement(By.name("end_date")).sendKeys("14-NOV-2021");
-    driver.findElement(By.name("case_type")).click();
-
-    driver.findElement(By.cssSelector("option:nth-child(17)")).click();
-    driver.findElement(By.cssSelector("input:nth-child(4)")).click();
-    driver.switchTo().frame(1);
-
-    driver.findElement(By.linkText("2109558")).click();
-    driver.switchTo().frame(1);
-    vars.put("courtDate", driver.findElement(By.cssSelector("a:nth-child(5) td:nth-child(2)")).getText());
-    vars.put("room", driver.findElement(By.cssSelector("a:nth-child(5) td:nth-child(3)")).getText());
-    vars.put("location", driver.findElement(By.cssSelector("a:nth-child(5) td:nth-child(4)")).getText());
-    System.out.println(vars.get("courtDate").toString());
+    new Scraper(driver).run();
   }
 }
